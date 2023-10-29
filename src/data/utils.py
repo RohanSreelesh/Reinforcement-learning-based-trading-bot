fa0a84666c39e5cbddb696753bdb6c8e7a8d1891
vinaycat <vinaymeldrum@gmail.com>
2023-10-29 09:46:42 -0400

    Solved

--- src/data/utils.py
+++ src/data/utils.py
@@ -6,4 +6,5 @@ def load_dataset(name, index_name):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(base_dir, "../..", "database", name + ".csv")
     df = pd.read_csv(path, parse_dates=True, index_col=index_name)
+    df = df.dropna()
     return df
