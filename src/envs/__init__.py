391ff752561d46144347e3a1a9bb8e73b074441b
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2023-12-06 20:46:55 -0500

    Datasets for testing performance

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,4 +1,6 @@
 from gymnasium.envs.registration import register
 from .trading import *
+from .trading2 import *
 
 register(id="trading-v1", entry_point="envs.trading:TradingEnv")
+register(id="trading-v2", entry_point="envs.trading2:TradingEnv1")
