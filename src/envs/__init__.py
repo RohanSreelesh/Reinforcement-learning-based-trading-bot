231f3a299ecfecf252dc8e6308e737117f3a0180
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2023-12-06 20:49:42 -0500

    Revert "Datasets for testing performance"
    This reverts commit 391ff752561d46144347e3a1a9bb8e73b074441b.

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,6 +1,4 @@
 from gymnasium.envs.registration import register
 from .trading import *
-from .trading2 import *
 
 register(id="trading-v1", entry_point="envs.trading:TradingEnv")
-register(id="trading-v2", entry_point="envs.trading2:TradingEnv1")
