99f6162fc16a0658d267214847172561125d2e24
anthony-y-zhu14 <41710998+anthony-y-zhu14@users.noreply.github.com>
2023-12-07 09:05:11 -0500

    - update parameter

--- src/algorithms/classes/helper.py
+++ src/algorithms/classes/helper.py
@@ -7,12 +7,12 @@ def setup_env_for_testing():
     env = gym.make(
         "trading-v1",
         data_frames=STOCKS.NASDAQ_TEST,
-        window_size=1,
+        window_size=5,
         render_mode=None,
-        start=10000,
-        goal=20000,
-        stop_loss_limit=5000,
-        max_shares_per_trade=1,
+        start=100000,
+        goal=200000,
+        stop_loss_limit=50000,
+        max_shares_per_trade=10,
     )
 
     trading_env: TradingEnv = env.unwrapped
