e310684d0efc6a1e230498cfa6744b1469622fc4
anthony-y-zhu14 <41710998+anthony-y-zhu14@users.noreply.github.com>
2023-12-07 21:36:03 -0500

    - update

--- src/algorithms/classes/linear_dyna_q_learner.py
+++ src/algorithms/classes/linear_dyna_q_learner.py
@@ -1,12 +1,9 @@
 import numpy as np
 from .linear_model import LinearModel
 
+
 class LinearDynaQLearner:
-    def __init__(self,
-                 num_features,
-                 num_actions,
-                 learning_rate=0.01,
-                 gamma=0.99):
+    def __init__(self, num_features, num_actions, learning_rate=0.01, gamma=0.99):
         self.num_actions = num_actions
         self.weights = np.random.randn(num_features, num_actions)
         self.learning_rate = learning_rate
@@ -16,16 +13,13 @@ class LinearDynaQLearner:
     def predict(self, state: np.ndarray):
         return np.dot(state.flatten(), self.weights)
 
-    def update(self, state: np.ndarray, next_state: np.ndarray, action: int,
-               reward: float, done: bool):
+    def update(self, state: np.ndarray, next_state: np.ndarray, action: int, reward: float, done: bool):
         Q_values_current = self.predict(state)
         Q_values_next = self.predict(next_state)
         best_next_action = np.max(Q_values_next)
         td_target = reward + (0 if done else self.gamma * best_next_action)
         td_error = td_target - Q_values_current[action]
-        self.weights[:,
-                     action] += self.learning_rate * td_error * state.flatten(
-                     )
+        self.weights[:, action] += self.learning_rate * td_error * state.flatten()
 
     def update_model(self, state, action, next_state, reward):
         self.model.update(state, action, next_state, reward)
--- src/algorithms/dyna_q.py
+++ src/algorithms/dyna_q.py
@@ -1,9 +1,8 @@
 import numpy as np
 import random
-from sklearn.preprocessing import StandardScaler
+from algorithms.classes.helper import setup_env_for_testing
 from envs import TradingEnv
 from models import Action
-import data as STOCKS
 from .classes.state_featurizer import StateFeaturizer
 from .classes.linear_dyna_q_learner import LinearDynaQLearner
 
@@ -23,46 +22,30 @@ class AgentActionHistory:
     def sample(self, batch_size):
         return random.sample(self.buffer, batch_size)
 
-# def epsilon_greedy_policy(agent, epsilon, env, state):
-#     if np.random.rand() < epsilon:
-#         action_mask = Action.get_action_mask(env)
-#         valid_actions = np.where(action_mask == 1)[0]
-#         return np.random.choice(valid_actions) - env.max_shares_per_trade
-#     else:
-#         q_values = agent.predict(state)
-#         action_mask = Action.get_action_mask(env)
-#         masked_q_values = np.where(action_mask == 1, q_values, -np.inf)
-#         return np.argmax(masked_q_values)
 
-def epsilon_greedy_policy(agent: LinearDynaQLearner, epsilon: float,
-                          env: TradingEnv, state: np.ndarray):
+def greedy_policy(agent: LinearDynaQLearner, env: TradingEnv, state: np.ndarray):
+    predictions = agent.predict(state)
+    action_mask = Action.get_action_mask(env)
+
+    predictions_with_valid_action = np.array(
+        [predictions[action] if Action.is_action_valid(action_mask, action) else -np.Infinity for action in np.arange(len(predictions))]
+    )
+
+    return np.argmax(predictions_with_valid_action)
+
+
+def epsilon_greedy_policy(agent: LinearDynaQLearner, epsilon: float, env: TradingEnv, state: np.ndarray):
     if np.random.rand() < epsilon:
         action_mask = Action.get_action_mask(env)
         # Random action
         return env.action_space.sample(mask=action_mask)
 
     # Greedy action
-    return np.argmax(agent.predict(state))
-    predictions = agent.predict(state)
-    action_mask = Action.get_action_mask(env)
-
-    predictions_with_valid_action = np.array([
-        predictions[action]
-        if Action.is_action_valid(action_mask, action) else -np.Infinity
-        for action in np.arange(len(predictions))
-    ])
-
-    return np.argmax(predictions_with_valid_action)
+    return greedy_policy(agent, env, state)
 
 
 def demo():
-    env = TradingEnv(data_frames=STOCKS.NASDAQ_TRAIN,
-                     window_size=30,
-                     render_mode="human",
-                     start=50000,
-                     goal=200000,
-                     stop_loss_limit=0,
-                     max_shares_per_trade=1000)
+    env, env_test = setup_env_for_testing()
 
     num_features = np.prod(env.observation_space.shape)
     num_actions = env.action_space.n
@@ -87,8 +70,7 @@ def demo():
             next_state, reward, terminated, truncated, _ = env.step(action)
             next_state = featurizer.transform(next_state)
 
-            agent.update(state, next_state, action, reward, terminated
-                         or truncated)
+            agent.update(state, next_state, action, reward, terminated or truncated)
             agent.update_model(state, action, next_state, reward)
 
             action_history.push(state, action, next_state, reward)
@@ -104,35 +86,21 @@ def demo():
                 break
 
     # Test loop
-    env_test = TradingEnv(data_frames=STOCKS.NASDAQ_TEST,
-                          window_size=30,
-                          render_mode="human",
-                          start=50000,
-                          goal=200000,
-                          stop_loss_limit=0,
-                          max_shares_per_trade=1000)
-
     state, _ = env_test.reset()
     state = featurizer.transform(state)
     done = False
 
     while not done:
-        action = epsilon_greedy_policy(agent=agent,
-                                        env=env_test,
-                                        state=state,
-                                        epsilon=0.1)
+        action = greedy_policy(
+            agent=agent,
+            env=env_test,
+            state=state,
+        )
 
-        next_state, reward, terminated, truncated, _ = env_test.step(
-            action)
+        next_state, reward, terminated, truncated, _ = env_test.step(action)
         next_state = state = featurizer.transform(next_state)
 
         done = terminated or truncated
-
-        agent.update(state=state,
-                      next_state=next_state,
-                      action=action,
-                      reward=reward,
-                      done=done)
 
         state = next_state
 
